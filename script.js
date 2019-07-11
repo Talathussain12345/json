802bfbf077493d35a09c99348999c300be076826
Talathussain12345 <49521244+Talathussain12345@users.noreply.github.com>
2019-07-11 17:43:34 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,113 @@
+let whatasAppData = [
+    {
+        name: "salman",
+        lastMsg: "hello",
+        cont: "23424567820"
+
+    },
+
+    {
+        name: "kashif",
+        lastMsg: "hi",
+        cont: "0987654321"
+    },
+    {
+        name: "salman",
+        lastMsg: "hy",
+        cont: "09878965432"
+    }
+
+]
+let techKaro = {
+
+    students: [
+        {
+            name: "kashif",
+            age: "23",
+            lastMsg: "hello",
+            cont: "23424567820"
+        },
+        {
+            name: "raheel",
+            age: "26",
+            lastMsg: "hello",
+            cont: "23424567820"
+
+        },
+        {
+            name: "raheel",
+            age: "26",
+            lastMsg: "hello",
+            cont: "23424567820"
+
+        },
+        {
+            name: "raheel",
+            age: "26",
+            lastMsg: "hello",
+            cont: "23424567820"
+
+        },
+    ],
+
+    teachers: [
+        {
+            name: "raheel",
+            age: "26",
+            cont: "23424567820"
+
+
+        },
+        {
+            name: "raheel",
+            age: "26",
+            lastMsg: "hello",
+            cont: "23424567820"
+
+        },
+        {
+            name: "raheel",
+            age: "26",
+            lastMsg: "hello",
+            cont: "23424567820"
+
+        },
+
+    ],
+
+    faculty: [
+        {
+            name: "raheel",
+            age: "26",
+            lastMsg: "hello",
+            cont: "23424567820"
+
+        },
+        {
+            name: "raheel",
+            age: "26",
+            lastMsg: "hello",
+            cont: "23424567820"
+
+        },
+        {
+            name: "raheel",
+            age: "26",
+            lastMsg: "hello",
+            cont: "23424567820"
+
+        },
+    ],
+
+
+
+
+}
+
+alert('kukjj')
+document.querySelector("p").innerHTML = `${whatasAppData}`;
+
+
+
+
+
